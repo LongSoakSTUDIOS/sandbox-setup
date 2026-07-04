52a2f3ad5ed06be956a0d86950f550d571e62ed5
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-04 22:08:34 +1000

    bashrc

---
+++ bashrc
@@ -0,0 +1,11 @@
+source $HOME/.bash_aliases;
+
+###Colors
+#CYAN="\[\e[38;2;25;249;216m\]";
+#GREEN="\[\e[38;2;93;213;3m\]";
+#YELLOW="\[\e[38;2;211;173;3m\]";
+#BLUE="\[\e[38;2;0;172;253m\]";
+#AE="\[\e[0m\]";
+#MPC=$CYAN;
+#
+#PS1="$MPC\u@sandbox:\w\n> $AE"
--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-test
+USERNAME="LongSoakSTUDIOS"
+NAME="Nicky B"
+USEREMAIL="longsoakSTUDIOS@gmail.com"
+REPO="holbertonschool-shell"
 
+#grab bashrc from repo and save
+if [[ -f ~/.bashrc ]]; then
+	mv $HOME/.bashrc $HOME/.bashrc.bak;
+fi
+wget -qO ~/.bashrc https://raw.githubusercontent.com/$USERNAME/sandbox-setup/refs/heads/main/bashrc
+				
